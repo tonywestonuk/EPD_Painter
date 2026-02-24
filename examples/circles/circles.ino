e06a96241976135d79d061ba358f9a4e622ed977
tony weston <tonyw@totspics.com>
2026-02-24 07:43:16 +0000

    Added circles example

---
+++ examples/circles/circles.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "EPD_Painter.h"
+#include "EPD_Painter_devices.h"
+
+EPD_Painter epd(epd_paper_dev_m5stacks3);
+
+
+
+void setup() {
+if (!epd.begin()) {
+    Serial.println("EPD init failed");
+    while (1);
+  }
+  epd.clear();
+  epd.clear();
+
+}
+
+void loop() {
+  epd.fillCircle(random(epd.width()), random(epd.height()), random(300), random(4) );
+  epd.paint();
+
+}
